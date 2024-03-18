79a4119c44352c807ee743bafb3b8e7b153a73bf
Daniel Grams <daniel.grams@jtl-software.com>
2024-03-18 08:51:20 +0100

    Initial commit

---
+++ MauiApp2/ModalPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp2
+{
+    public partial class ModalPage : ContentPage
+    {
+        public ModalPage()
+        {
+            InitializeComponent();
+            this.listView.ItemsSource = this.Objects;
+        }
+
+        public object[] Objects { get; set; } = {"1", "2", "3"};
+
+
+        private void MenuItem_OnClicked(object? sender, EventArgs e)
+        {
+            Console.WriteLine("jo");
+        }
+
+        private async void Button_OnClicked(object? sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+
+}
